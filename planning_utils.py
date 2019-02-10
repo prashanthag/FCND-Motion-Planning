c51ea0ab3dde8bdafe6576f9c4cf6d231864bd7c
Prashantha gowda <prashantha.g@gmail.com>
2019-02-09 18:54:39 -0800

    * added to support diagonal movement in A*

--- planning_utils.py
+++ planning_utils.py
@@ -55,6 +55,10 @@ class Action(Enum):
     EAST = (0, 1, 1)
     NORTH = (-1, 0, 1)
     SOUTH = (1, 0, 1)
+    SOUTH_WEST = (-1, -1, np.sqrt(2))
+    NORTH_WEST = (1, -1, np.sqrt(2))
+    SOUTH_EAST = (-1, 1, np.sqrt(2))
+    NORTH_EAST = (1, 1, np.sqrt(2))
 
     @property
     def cost(self):
@@ -84,6 +88,14 @@ def valid_actions(grid, current_node):
         valid_actions.remove(Action.WEST)
     if y + 1 > m or grid[x, y + 1] == 1:
         valid_actions.remove(Action.EAST)
+    if y - 1 < 0 or x - 1 < 0 or grid[x - 1, y - 1] == 1:
+	        valid_actions.remove(Action.SOUTH_WEST)
+    if y - 1 < 0 or x + 1 > n or grid[x + 1, y - 1] == 1:
+	        valid_actions.remove(Action.NORTH_WEST)
+    if y + 1 > m or x + 1 > n or grid[x + 1, y + 1] == 1:
+	        valid_actions.remove(Action.NORTH_EAST)
+    if y + 1 > m or x - 1 < 0 or grid[x - 1, y + 1] == 1:
+	        valid_actions.remove(Action.SOUTH_EAST)
 
     return valid_actions
 
